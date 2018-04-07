a65cfa3a4adef67f8d720d7a08395dc158f69a87
濱野将士 <masashihamano@mbp.local>
2018-04-07 14:56:43 +0900

    first commit

---
+++ users_controller.rb
@@ -0,0 +1,17 @@
+
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    # binding.pry
+    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    if @user.save
+      redirect_to root_path, notice: '登録が完了しました'
+    else
+      flash.now[:alert] = "登録に失敗しました"
+      render :new
+    end
+  end
+end
